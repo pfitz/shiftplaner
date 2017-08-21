88fbbc2e2339089c8906e275e1e1842d8c0efab7
Friedrich Pfitzmann <friedrich@pfiss.com>
2017-08-21 11:51:23 +0200

    Added events to shiftplaner module

--- lib/shiftplaner.ex
+++ lib/shiftplaner.ex
@@ -3,16 +3,76 @@ defmodule Shiftplaner do
   Documentation for Shiftplaner.
   """
 
+  ##################################################################
+  ####
+  ####          Event functions
+  ####
+  ##################################################################
+
   @doc """
-  Hello world.
+  creates a changeset for the given ```event```
 
-  ## Examples
+  event = An event of type ```%Shiftplaner.Event{}```
 
-      iex> Shiftplaner.hello
-      :world
-
+  Returns an ```%Ecto.Changeset{}``` for the ```%Shiftplaner.Event{}```
   """
-  def hello do
-    :world
-  end
+  @spec change_event(Shiftplaner.Event.t) :: Ecto.Changeset.t
+  defdelegate change_event(event), to: Shiftplaner.Event
+
+  @doc """
+  Tries to create an event from the given ```attrs```.
+
+  Returns either ```{:ok, event}``` or ```{:error, changeset}```
+  """
+  @spec create_event(map) :: {:ok, Shiftplaner.Event.t} | {:error, Ecto.Changeset.t}
+  defdelegate create_event(attrs), to: Shiftplaner.Event
+
+  @doc """
+  It returns {:ok, event} if the struct has been successfully deleted or {:error, changeset}
+  if there was a validation or a known constraint error.
+  """
+  @spec delete_event(Shiftplaner.Event.t) :: {:ok, Shiftplaner.Event.t} | {:error, Ecto.Changeset.t}
+  defdelegate delete_event(event), to: Shiftplaner.Event
+
+  @doc """
+  Tries to get the event for the given binary uuid.
+
+  Returns either an Event or nil or nothing.
+  """
+  @spec get_event(String.t) :: Shiftplaner.Event.t | nil | no_return
+  defdelegate get_event(id), to: Shiftplaner.Event
+
+  @doc """
+  Lists all active and inactive events and preloads the weekends.
+
+  Returns a list of ```Shiftplaner.Event```
+  """
+  @spec list_all_events :: list(Shiftplaner.Event.t)
+  defdelegate list_all_events, to: Shiftplaner.Event
+
+  @doc """
+  Updates the given ```Event```.
+
+  If successful returns ```{:ok, updated_event}```.
+  If unsuccesful returns ```{:error, changeset}```.
+  """
+  @spec update_event(Shiftplaner.Event.t, map) :: {:ok, Shiftplaner.Event} | {
+    :error,
+    Ecto.Changeset.t
+  }
+  defdelegate update_event(event, attrs), to: Shiftplaner.Event
+
+  ##################################################################
+  ####
+  ####          Person functions
+  ####
+  ##################################################################
+
+  @doc """
+  List all persons
+
+  Returns a list of ```Shiftplaner.Person```
+  """
+  @spec list_persons :: list(Shiftplaner.Person.t)
+  defdelegate list_persons, to: Shiftplaner.Person
 end
