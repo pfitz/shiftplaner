b4e958182df116abd070fbc284d104fc4c304ff9
Friedrich Pfitzmann <friedrich@pfiss.com>
2017-08-21 11:49:49 +0200

    Added Days

---
+++ lib/shiftplaner/day.ex
@@ -0,0 +1,112 @@
+defmodule Shiftplaner.Day do
+  @moduledoc false
+
+  use Ecto.Schema
+
+  alias Shiftplaner.{Day, Repo, Shift, Weekend}
+
+  import Ecto.{Query, Changeset}, warn: false
+
+  require Logger
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type Ecto.UUID
+
+  @type t :: %__MODULE__{
+               date: Date.t,
+               weekend: Shiftplaner.Weekend.t,
+               shifts: list(Shiftplaner.Shift.t)
+             }
+  schema "day" do
+    field :date, :date
+
+    belongs_to :weekend, Weekend
+    has_many :shifts, Shift
+
+    timestamps()
+  end
+
+  ##################################################################
+  ####
+  ####          Public API
+  ####
+  ##################################################################
+
+  @spec add_shift(
+          Shiftplaner.Day.t,
+          Shiftplaner.Shift.t | list(Shiftplaner.Shift.t)
+        ) :: {
+               :ok,
+               Shiftplaner.Day.t
+             } | {
+               :error,
+               Ecto.Changeset.t
+             }
+  def add_shift(%Day{} = day, %Shift{} = shift) do
+    day
+    |> day_changeset(%{})
+    |> put_assoc(:shifts, [shift])
+    |> Repo.update()
+    |> update_result()
+  end
+
+  def add_shift(%Day{} = day, list_of_shifts) when is_list(list_of_shifts) do
+    Enum.each(list_of_shifts, &add_shift(day, &1))
+  end
+
+  @spec change_day(Shiftplaner.Day.t) :: Ecto.Changeset.t
+  def change_day(%Day{} = day) do
+    day_changeset(day, %{})
+  end
+
+  @spec create_day(map) :: Shiftplaner.Day.t | nil
+  def create_day(attrs) do
+    %Day{}
+    |> day_changeset(attrs)
+    |> Repo.insert()
+    |> insert_result
+    |> Repo.preload(:shifts)
+  end
+
+  @spec list_days :: list(Shiftplaner.Day.t)
+  def list_days do
+    Day
+    |> Repo.all
+  end
+
+  ##################################################################
+  ####
+  ####          Private functions
+  ####
+  ##################################################################
+
+  defp day_changeset(%Day{} = day, attrs) do
+    day
+    |> Repo.preload(:weekend)
+    |> cast(attrs, [:date])
+    |> validate_required([:date])
+    |> cast_assoc(:weekend)
+  end
+
+  defp insert_result({:ok, %Day{} = day}) do
+    Logger.debug fn -> "successfully inserted day - #{day.date}" end
+    day
+  end
+
+  defp insert_result({:error, reason}) do
+    Logger.warn fn -> "Could not insert Day - #{reason}" end
+    nil
+  end
+
+  defp update_result({:ok, %Day{} = day}) do
+    Logger.debug fn ->
+      "successfully updated day - #{day.id}: #{day.date}"
+    end
+    day
+  end
+
+  defp update_result({:error, reason}) do
+    Logger.warn fn -> "Could not update day - #{reason}" end
+    nil
+  end
+end
